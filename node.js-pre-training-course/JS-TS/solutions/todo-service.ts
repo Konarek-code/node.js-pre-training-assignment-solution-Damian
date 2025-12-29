83d06ff69f2e77bca0371bc6bbb50ee60b49059d
unknown <damian.konarski71@gmail.com>
2025-12-29 19:44:02 +0100

    1 and 2 task done

--- node.js-pre-training-course/JS-TS/solutions/todo-service.ts
+++ node.js-pre-training-course/JS-TS/solutions/todo-service.ts
@@ -1,18 +1,40 @@
-import { TodoApi } from './todo-api';
-import { Todo } from './types';
+import { TodoApi } from "./todo-api";
+import { Todo, TodoStatus } from "./types";
 
 export class TodoService {
-  constructor(private readonly api: TodoApi) { }
+  constructor(private readonly api: TodoApi) {}
 
-  async create(title: string, description = ''): Promise<Todo> {
-    throw new Error('create: not implemented');
+  async create(title: string, description = ""): Promise<Todo> {
+    const newTodo = { title, description, status: TodoStatus.PENDING };
+    const createdTodo = await this.api.add(newTodo);
+    return createdTodo;
   }
 
   async toggleStatus(id: number): Promise<Todo> {
-    throw new Error('toggleStatus: not implemented');
+    const todo = await this.api.getById(id);
+    if (!todo) throw new Error(`Todo with id ${id} not found`);
+
+    let newStatus: TodoStatus;
+    switch (todo.status) {
+      case TodoStatus.PENDING:
+        newStatus = TodoStatus.IN_PROGRESS;
+        break;
+      case TodoStatus.IN_PROGRESS:
+        newStatus = TodoStatus.COMPLETED;
+        break;
+      case TodoStatus.COMPLETED:
+        newStatus = TodoStatus.PENDING;
+        break;
+      default:
+        newStatus = TodoStatus.PENDING;
+        break;
+    }
+
+    const updated = await this.api.update(id, { ...todo, status: newStatus });
+    return updated;
   }
 
   async search(keyword: string): Promise<Todo[]> {
-    throw new Error('search: not implemented');
+    return await this.api.search(keyword);
   }
 }
--- node.js-pre-training-course/JS-TS/solutions/types.ts
+++ node.js-pre-training-course/JS-TS/solutions/types.ts
@@ -1,9 +1,18 @@
 interface Todo {
-
+  id: number;
+  title: string;
+  description?: string;
+  status: TodoStatus;
+  readonly createdAt: Date;
 }
 
 enum TodoStatus {
-
+  PENDING,
+  IN_PROGRESS,
+  COMPLETED,
 }
+type NewTodo = Omit<Todo, "id" | "createdAt">;
+
+export type { NewTodo };
 
 export { Todo, TodoStatus };
